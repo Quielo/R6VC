ebef078e7bf6d661a798fd7d282a70a9f9ce7256
Tozarf <Fazatojuni@gmail.com>
2021-07-01 19:22:28 -0500

    pop-up windows with hovers

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import styles from "./App.module.scss";
 
 function room() {
   return (
-    <div className={styles.room}>
-      <div className={styles.welcomecontainer}>
+    <>
+      <div className={styles.linkPopUp}>
+        <h3 className={styles.permission}>Permissions</h3>
+        <p className={styles.instructions}>
+          Edit the name of the room related to the topic of the meeting. As you
+          wish! Share the code with your guests. And the meeting is ready!
+        </p>
+        <div className={styles.urlContainer}>
+          <h3 className={styles.url}>_____Url_here!_____</h3>
+          <div className={styles.copyIcon}></div>
+        </div>
+      </div>
+      <div className={styles.room}>
+        {/* <div className={styles.welcomecontainer}>
         <p className={styles.welcomemessage}>
           {" "}
           Welcome!
@@ -21,33 +33,34 @@ function room() {
         <button id="buttonOk" className={styles.okButton}>
           OK!
         </button>
-      </div>
+      </div> */}
 
-      <div className={styles.roomcontainer}>
-        <div className={styles.roomname}>
-          <p>ROOM NAME</p>
-        </div>
-        <div className={styles.logodown}></div>
-        <div className={styles.mic}></div>
-        <div className={styles.arrow}></div>
-        <div className={styles.userslist}></div>
-        <div className={styles.user1}>
-          <div className={styles.usernamecontainer}>
-            <p className={styles.username}>NAME</p>
+        <div className={styles.roomcontainer}>
+          <div className={styles.roomname}>
+            <p>ROOM NAME</p>
           </div>
-        </div>
-        <div className={styles.user2}>
-          <div className={styles.usernamecontainer}>
-            <p className={styles.username}>NAME</p>
+          <div className={styles.logodown}></div>
+          <div className={styles.mic}></div>
+          <div className={styles.arrow}></div>
+          <div className={styles.userslist}></div>
+          <div className={styles.user1}>
+            <div className={styles.usernamecontainer}>
+              <p className={styles.username}>NAME</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.user3}>
-          <div className={styles.usernamecontainer}>
-            <p className={styles.username}>NAME</p>
+          <div className={styles.user2}>
+            <div className={styles.usernamecontainer}>
+              <p className={styles.username}>NAME</p>
+            </div>
+          </div>
+          <div className={styles.user3}>
+            <div className={styles.usernamecontainer}>
+              <p className={styles.username}>NAME</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
